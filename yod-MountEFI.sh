2cf08291e3732d267e67371a9f52fc8e058f877d
cecekpawon <killprie@gmail.com>
2015-10-25 22:39:55 +0700

    Mount EFI: add "-a" param as auto mount

--- yod-MountEFI.sh
+++ yod-MountEFI.sh
@@ -9,14 +9,24 @@ gME="@cecekpawon | thrsh.net"
 
 gHEAD=`cat <<EOF
 ${gTITLE}: ${gME}
-============================================
+=============================================
+Auto mount startup disk: ./${0##*/} -a
 Inspiration: Clover Configurator, #longlive!
---------------------------------------------\n\n
+---------------------------------------------\n\n
 EOF`
 
 gMSG=`cat <<EOF
 Please die!
 EOF`
+
+gAuto=false
+aDisk=()
+aPar=()
+aLabel=()
+gStr=""
+i=0
+C_BLUE="\e[38;5;27m"
+C_BLACK="\e[0m"
 
 clear && printf "${gHEAD}"
 
@@ -30,13 +40,9 @@ if [[ $gEFITotal -eq 0 ]]; then
   exit
 fi
 
-#aDisk=()
-aPar=()
-aLabel=()
-gStr=""
-i=0
-C_BLUE="\e[38;5;27m"
-C_BLACK="\e[0m"
+[[ "${1}" == "-a" ]] && gAuto=true
+
+gStartup="$(diskutil info / | awk '/Identifier/' | grep -o -e disk[[:digit:]])"
 
 printf "`cat <<EOF
 Getting EFI disks ..
@@ -44,12 +50,10 @@ Getting EFI disks ..
 Choose one from available devices:
 [${C_BLUE}#${C_BLACK}] is current startup disk!
 
-----+-----------+---------------------------
+----+-----------+----------------------------
 [#]\t| Partition\t| Label
-----+-----------+---------------------------\n
+----+-----------+----------------------------\n
 EOF`"
-
-gStartup="$(diskutil info / | awk '/Identifier/' | grep -o -e disk[[:digit:]])"
 
 for gArg in "${aEFI[@]}"
 do
@@ -57,25 +61,32 @@ do
   gInfo=$(diskutil info "${gDevice}" | awk '/Media Name:/' | sed -e 's/^.*://' | sed 's/^ *//;s/ *Media//')
 
   aPar+=("${gArg}")
-  #aDisk+=("${gDevice}")
+  aDisk+=("${gDevice}")
   aLabel+=("${gInfo}")
+  C_HI=$C_BLACK
 
-  [[ "${gStartup}" == "${gDevice}" ]] && C_HI=$C_BLUE || C_HI=$C_BLACK
+  if [[ "${gStartup}" == "${gDevice}" ]]; then
+    C_HI=$C_BLUE
+    [[ $gAuto == true ]] && gChoose=$i
+  fi
 
   gStr+="[${C_HI}${i}${C_BLACK}]\t| ${C_HI}${gArg}${C_BLACK}\t\t| ${C_HI}${gInfo}${C_BLACK}\n"
 
   let i++
 done
 
-printf "${gStr}\nChoose [#]: "
-read gChoose
+printf "${gStr}\n"
+
+[[ $gAuto != true ]] && read -p "Choose [#]: " gChoose
 
 if [[ "${gChoose}" =~ ^[[:digit:]]+$ ]] && [[ $gChoose -lt $gEFITotal ]]; then
+  gDisk=${aDisk[$gChoose]}
   gPar=${aPar[$gChoose]}
   gLabel=${aLabel[$gChoose]}
   gOEFI=$(df | grep "${gPar}")
   mEFI="/Volumes/EFI(${gPar})"
 
+  [[ $gAuto == true ]] && printf "Auto mount EFI on: ${gDisk} .."
   printf "\nMounting ${gPar} (on ${gLabel}) ..\n\n"
 
   [[ ! -z "${gOEFI}" ]] && diskutil unmount "${gPar}"
